23cabd5240cff5e9f1e66e364712b7174a309708
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-09-06 12:50:16 -0600

    Add files via upload

---
+++ ImprimirDigitosLocos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	int i;
+	int u=0;
+	int r;
+	
+	printf("Ingrese numero\n");
+	scanf("%d", &i);
+	
+	while(u<=8)
+	{
+		i++;
+		printf("%d\n", i);
+		u++;
+	}
+	
+	
+	return 0;
+}
