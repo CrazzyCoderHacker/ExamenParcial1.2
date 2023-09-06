edeeb5e4739f4ad6d50b924455c6678da755b777
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-09-06 11:53:17 -0600

    Add files via upload

---
+++ DetectorDeNegativo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+
+int main(int argc, char** argv) 
+{
+	
+	int titulo, i, r=1, u;
+	
+	for(titulo=1; titulo<=50; titulo++)
+	{
+		printf(" ");
+		if(titulo==50)
+		{
+			printf("Detector de Negativos\n");
+		}
+	
+	}
+	while(r<=10)
+	{
+	printf("Ingrese un numero\n");
+	scanf("%d", &i);
+     	if(i<0)
+	     {
+		
+		 printf("Es negativo papito\n");
+	     }
+	     r++;
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
