8c31c2636eb067ac6a6c16256b4be87dfc651bce
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-09-07 09:10:03 -0600

    Add files via upload

---
+++ TablaMultiplicarWhile.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	
+	int x;
+	int y;
+	int r;
+	
+    x = 1;
+    y = 1;
+	while(x<=10)
+	{ 
+	  while(y<=10)
+	  {
+	  	r= x * y;
+	  	printf("%d x %d = %d \n", x, y, r);
+	  	y = y + 1; 
+	  }
+	 x = x+1;
+	 y = 1;
+	 
+	}
+	
+    
+  
+	
+
+	
+	
+	return 0;
+}
