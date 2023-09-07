774969e0236fca2a9118cc627c6fabc7327bba01
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-09-07 10:08:11 -0600

    Update TablaDeMultiplicar.cpp

--- TablaDeMultiplicar.cpp
+++ TablaDeMultiplicar.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//no hacer caso a este codigo//
 
 int main(int argc, char** argv) 
 {
